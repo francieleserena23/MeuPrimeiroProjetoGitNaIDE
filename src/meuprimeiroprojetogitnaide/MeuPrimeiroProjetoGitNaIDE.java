20b89f8418bc86d97ffb040f4d109c4b419d3999
Franciele Serena <francieleserena@hotmail.com>
2019-10-18 20:19:00 -0300

    Commit inicial do projeto

--- src/meuprimeiroprojetogitnaide/MeuPrimeiroProjetoGitNaIDE.java
+++ src/meuprimeiroprojetogitnaide/MeuPrimeiroProjetoGitNaIDE.java
@@ -17,6 +17,7 @@ public class MeuPrimeiroProjetoGitNaIDE {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println("Sejam bem vindo ao GIT no NetBeans");
     }
     
 }
