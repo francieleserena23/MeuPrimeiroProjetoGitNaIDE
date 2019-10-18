34e3743de0a564d9d2cc5a951375a14170aa387b
Franciele Serena <francieleserena@hotmail.com>
2019-10-18 20:08:06 -0300

    Commit inicial do projeto

---
+++ src/meuprimeiroprojetogitnaide/MeuPrimeiroProjetoGitNaIDE.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuprimeiroprojetogitnaide;
+
+/**
+ *
+ * @author Fran-pc
+ */
+public class MeuPrimeiroProjetoGitNaIDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
